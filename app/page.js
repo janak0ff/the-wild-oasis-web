0c2c9a713b5ed586b0270adad54a850390775b63
Janak kr Shrestah <janakkrshrestha@gmail.com>
2025-04-11 22:10:29 +0545

    Creating a Layout

--- app/page.js
+++ app/page.js
@@ -4,9 +4,7 @@ import Navigation from "./components/Navigation";
 export default function Page() {
   return (
     <div>
-      <Navigation />
       <h1>Hello Next!</h1>
-      <Link href={"/about"}>About</Link>
       <Link href="/cabins">Cabins</Link>
     </div>
   );

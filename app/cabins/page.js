7a14fbb0c361bc4d0ef8c1ceedaaca70c5eb8280
Janak kr Shrestah <janakkrshrestha@gmail.com>
2025-04-12 20:01:27 +0545

    Project Organization

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,20 +1,10 @@
-import Counter from "../components/counter";
+import Counter from "@app/_components/counter";
 
 export default async function Page() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
-  const data = await res.json();
   return (
     <div>
       <h1>Cabins</h1>
       <p>Cabins page</p>
-
-      <ul>
-        {data.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
-
-      <Counter users={data} />
     </div>
   );
 }

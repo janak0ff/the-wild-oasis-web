fcccc2e2cdcc25f1b0315edfac6bde8a971bbaf7
Janak kr Shrestah <janakkrshrestha@gmail.com>
2025-04-12 21:04:03 +0545

    Adding Page Metadata and Favicon

--- app/layout.js
+++ app/layout.js
@@ -3,8 +3,13 @@ import Navigation from "./_components/Navigation";
 import "@/app/_styles/globals.css";
 
 export const metadata = {
-  title: "The wild oasis",
-  description: "visit world's best cabins",
+  // title: "The wild oasis",
+  title: {
+    default: "The wild oasis",
+    template: "%s | The wild oasis",
+  },
+  description: "visit world's best cabins hotels in the wild",
+  keywords: "wild, oasis, cabins, hotels, travel",
 };
 
 export default function RootLayout({ children }) {

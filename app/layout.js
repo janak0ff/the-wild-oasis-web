8bb1d52dd1b2bb54a1a9adf72244f607db30726a
Janak kr Shrestah <janakkrshrestha@gmail.com>
2025-04-12 21:17:45 +0545

    Loading and Optimizing Fonts

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,16 @@ import Logo from "./_components/Logo";
 import Navigation from "./_components/Navigation";
 import "@/app/_styles/globals.css";
 
+// import fonts from "next/font/google";
+import { Josefin_Sans } from "next/font/google";
+
+//configure the font
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap", // fallback to system fonts
+  weight: ["400", "700"],
+});
+
 export const metadata = {
   // title: "The wild oasis",
   title: {
@@ -15,7 +25,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-primary-100 min-h-screen">
+      <body
+        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+      >
         <header>
           <Logo />
           <Navigation />
